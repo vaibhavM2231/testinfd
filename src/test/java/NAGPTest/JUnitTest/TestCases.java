9ee23398b8b5c7f0239afd53f3c948e75e893fa9
vaibhavM2231 <vaibhav.mishra1993@outlook.com>
2024-05-25 21:34:37 +0530

    initial commit

---
+++ src/test/java/NAGPTest/JUnitTest/TestCases.java
@@ -0,0 +1,27 @@
+package NAGPTest.JUnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCases {
+
+	@Test
+	public void test() {
+		
+		
+		int sum = addNumbers(10, 20);
+		
+		int expected= 30;
+		
+		assertEquals(sum, expected);
+	}
+	
+	
+	public int addNumbers(int a, int b) {
+		int c = a+b;
+		
+		return c;
+	}
+
+}
